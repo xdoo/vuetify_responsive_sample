7b1bcead6ac90dde4a0863707ab5a2b51d3f587a
xdoo <claus.straube@gmail.com>
2020-05-07 14:22:18 +0200

    a dummy form

---
+++ src/components/Form.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+            label="A Field"
+          ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>  
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from "vue-property-decorator"
+
+@Component
+export default class Form extends Vue{
+  
+}
+</script>

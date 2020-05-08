498c9248f99daa561e74ccaa84340dd3d8dda0af
xdoo <claus.straube@gmail.com>
2020-05-08 09:06:55 +0200

    some extra features

--- src/components/ResponsiveFont.vue
+++ src/components/ResponsiveFont.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row class="pa-3">
-    <h1 class="font-weight-black text-h3 text-md-h2 text-lg-h1 text-xl-h1">This is my heading</h1>
-    <p class="text--secondary"> 
+    <h1 class="font-weight-black text-h3 text-md-h2 text-lg-h1 text-xl-h1 text-truncate">This is my heading</h1>
+    <p class="text--secondary text-caption text-sm-body-2 text-md-body-1 text-lg-body-1 text-xl-body-1 text-justify"> 
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
     </p>
   </v-row>

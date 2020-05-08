5685246bdee0aecc3eda8a50b05d6a395ddeab5b
xdoo <claus.straube@gmail.com>
2020-05-08 08:10:45 +0200

    created own component for text sample

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,7 @@
 
     <v-content>
       <v-container>
-        <v-row class="pa-3">
-          <h1 class="text-display-2 text-md-title">
-            This is my Heading
-          </h1>
-        </v-row>
-        <v-row class="pa-3">
-          <p class="text--secondary"> 
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
-          </p>
-        </v-row>
+      <responsive-font></responsive-font>
       <Form></Form>
       </v-container>
     </v-content>
@@ -33,11 +24,13 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from "vue-property-decorator"
-import Form from './components/Form.vue'
+import Form from '@/components/Form.vue'
+import ResponsiveFont from "@/components/ResponsiveFont.vue"
 
 @Component({
   components: {
-    Form
+    Form,
+    ResponsiveFont
   }
 })
 export default class App extends Vue {
---
+++ src/components/ResponsiveFont.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row class="pa-3">
+    <h1 class="font-weight-black text-h3 text-md-h2 text-lg-h1 text-xl-h1">This is my heading</h1>
+    <p class="text--secondary"> 
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+    </p>
+  </v-row>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from "vue-property-decorator"
+
+@Component
+export default class ResponsiveFont extends Vue{
+    
+}
+</script>
